ba96a55cb1455b603e651d086c738aecee5457f0
samarthsv <59997898+samarthsv@users.noreply.github.com>
2020-01-17 16:32:20 +0530

    Add files via upload

---
+++ try.py
@@ -0,0 +1,14 @@
+import operator
+from goto import goto , label
+oper={'+':operator.add,'-':operator.sub,'*':operator.mul,'/':operator.truediv}
+label:retry
+try :
+ num = list(map(int, input().rstrip().split())) 
+ a=num[0]
+ b=num[1]
+except:
+ goto:retry
+
+print('enter \n+ for add\n- for minus\n* for multiplication \n/ for divide\n')
+select=str(input())
+print(oper[select](a,b))
